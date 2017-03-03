861883a862a949ca4e2cb397501bbdcd00827bf3
Noah <noahpeterham@gmail.com>
2017-03-03 15:59:14 -0500

    filled out comment route. Still missing view

--- routes/comment.js
+++ routes/comment.js
@@ -8,5 +8,12 @@ router.get('/:tweetid', function(req, res, next) {
         res.render('details', { title: 'Tweet' ,tweet: doc});
      });
 });
+router.post('/:tweetid', function(req, res, next){
+	Tweet.update({_id:req.params.tweetid},{$addToSet: {comment: req.params.comment}, {}, function (req, err, doc) {
+			Tweet.findOne({_id:req.params.tweetid}, function (err, doc) {
+				res.render('details', { title: 'Tweet' ,tweet: doc});
+		 });
+     });
+});
 
 module.exports = router;
