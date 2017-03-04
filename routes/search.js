e243c0affa43b0dd562a50e3c5b93cde518cbe6d
Noah <noahpeterham@gmail.com>
2017-03-03 19:59:10 -0500

    the rest of the comments

--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,4 @@
+// libraries
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose')
@@ -5,7 +6,9 @@ var mongoose = require('mongoose')
 
 router.get('/', function(req, res, next) {
     console.log(req.query.search['value']);
-    Tweet.paginate({"Tweet content":{ "$regex": ''+req.query.search['value']} },{ lean: true, offset: parseInt(req.query.start),
+    
+	// allows pagination based on record counts
+	Tweet.paginate({"Tweet content":{ "$regex": ''+req.query.search['value']} },{ lean: true, offset: parseInt(req.query.start),
         limit: parseInt(req.query.length) }).then(function(result) {
         var data = {'data':result.docs,'recordsTotal':result.total,'recordsFiltered':result.total,'draw': parseInt(req.query.draw)};
         res.json(data);
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,21 +1,26 @@
+// libraries
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose')
     , Tweet = mongoose.model('Tweet');
 
+// renders a detailed view of a specfic tweet using a document from the mongo store	
 router.get('/:tweetid', function(req, res, next) {
     Tweet.findOne({_id:req.params.tweetid}, function (err, doc) {
         res.render('details', { title: 'Tweet' ,tweet: doc});
      });
 });
 
+// renders the comment page
 router.get('/:tweetid/comment', function(req, res, next) {
     res.render('comment', { title: 'Add Comment', tweet:req.params.tweetid});
 });
 
+// inserts a tweet and then rerenders it on the client
 router.post('/:tweetid/comment', function(req, res, next){
-
+	
     Tweet.findOne({_id:req.params.tweetid}, function (err, doc) {
+        // creates a comments field if there isn't one already there
         if(!doc['Comments']){
             doc['Comments']=[];
             doc['Comments'].push(req.body.comment);
